ec3b1702f3c9bc395393024f9124580e1b8a3d8f
Mats Blomdahl <mats.blomdahl@gmail.com>
2022-08-27 11:48:14 +0200

    Add `front-page.php` "Under Construction" Page (#13)
    Changes:
    - Update `style.css` with additional headings for www.mkdevops.se customizations
    - New `template-parts/content-front-page` page with "Under Construction" elements
    - Basic background styling for "Under Construction" page
    - Remove outdated theme references from `.wp-env.json`

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title front-page"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
