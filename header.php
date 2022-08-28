495d7ce279d1e665879513cab6745785682dca08
Mats Blomdahl <mats.blomdahl@gmail.com>
2022-08-28 08:09:46 +0200

    Corrections to PR #14
    - Duplicate `head.title` tag and broken stylesheet link removed from `header.php` (now same as Underscores.me version)
    - Dynamic WordPress `head.meta.charset` attribute in `header.php` (restored to Underscores.me version)
    - Style corrections for getting rid of vertical scroll bar on "Under Construction" front page #13
      - Site title element hidden from site-branding section
      - No margin on body for WP "home"

--- header.php
+++ header.php
@@ -13,14 +13,12 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-        <meta charset="UTF-8" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&family=Orbitron:wght@700&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/styles.css" />
-        <title>IT Services When your company want to focus on business instead of IT!</title>
 
 	<?php wp_head(); ?>
 </head>
